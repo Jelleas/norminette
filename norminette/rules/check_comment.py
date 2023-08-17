095baf9d17c884f09936efe0e3c1b57eddc4b877
jelleas <jelle.van.assema@gmail.com>
2023-08-17 16:15:07 +0200

    don't allow comments on same line as code, don't count comments as lines of code

--- norminette/rules/check_comment.py
+++ norminette/rules/check_comment.py
@@ -12,12 +12,18 @@ class CheckComment(Rule):
         """
         Comments are only allowed in GlobalScope.
         """
+        is_start_of_line = context.peek_token(0).pos[1] == 0
         i = context.skip_ws(0)
+
+        has_code = is_start_of_line
         has_comment = False
         while (
             context.peek_token(i) is not None
             and context.check_token(i, "NEWLINE") is False
         ):
+            if not context.check_token(i, ["COMMENT", "MULT_COMMENT"]):
+                has_code = True
+
             if context.check_token(i, allowed_on_comment) is False:
                 if has_comment is True:
                     context.new_error("COMMENT_ON_INSTR", context.peek_token(i))
@@ -28,6 +34,8 @@ class CheckComment(Rule):
                 #     or context.history[-1] == "IsFuncDeclaration"
                 # ):
                 #     context.new_error("WRONG_SCOPE_COMMENT", context.peek_token(i))
+                if has_code:
+                    context.new_error("COMMENT_ON_INSTR", context.peek_token(i))
                 has_comment = True
             i += 1
         i = context.skip_ws(0)
--- norminette/rules/check_line_count.py
+++ norminette/rules/check_line_count.py
@@ -11,12 +11,15 @@ class CheckLineCount(Rule):
         """
         Each function can only have 25 lines between its opening and closing brackets
         """
+        has_code = False
         for t in context.tokens[: context.tkn_scope]:
             if t.type == "NEWLINE" or t.type == "ESCAPED_NEWLINE":
-                context.scope.lines += 1
-
+                if has_code:
+                    context.scope.lines += 1
+                has_code = False
+            elif t.type not in ["COMMENT", "MULT_COMMENT", "SPACE", "TAB"]:
+                has_code = True
         if type(context.scope) is GlobalScope:
-
             if context.get_parent_rule() == "CheckFuncDeclarations" and context.scope.lines > 25:
                 context.new_error("TOO_MANY_LINES", context.tokens[context.tkn_scope])
             return False, 0
