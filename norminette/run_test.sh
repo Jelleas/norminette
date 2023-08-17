0daff94faaff673f8fa6d2181217ed66fa7806fb
jelleas <jelle.van.assema@gmail.com>
2023-08-17 11:58:43 +0200

    rm check_assignation_indent

--- norminette/run_test.sh
+++ norminette/run_test.sh
@@ -1,11 +1,10 @@
-#!/bin/env sh
-
 python3 --version
 set -ex
 
 echo "Running lexer unit test:"
-python3 -m unittest discover tests/lexer/unit-tests/ "*.py"
+python3 -m unittest discover tests/lexer "*.py"
 echo "Running lexer test on files:"
-python3 -m  tests.lexer.files.file_token_test
-python3 -m tests.lexer.errors.tester
-python3 -m tests.rules.rule_tester
+# python3 -m  tests.lexer.files.file_token_test
+# python3 -m tests.lexer.errors.tester
+# python3 -m tests.rules.rule_tester
+pytest tests/rules
